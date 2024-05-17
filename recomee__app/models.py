c89a37817f8bcce1c8114ff2632234b7378696c4
Fritz Gerald Castillo <fritzgerald658@gmail.com>
2024-05-17 21:21:58 +0800

    database added, accomplished history page.

--- recomee__app/models.py
+++ recomee__app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Username(models.Model):
+    name = models.CharField(max_length=100, null= True)
+
+class InputResults(models.Model):
+    user_name = models.ForeignKey(Username, on_delete=models.CASCADE, null= True)
+    user_course = models.CharField(max_length=100, null=True)
+    user_industry= models.CharField(max_length=100, null=True)
+    user_skills= models.CharField(max_length=100, null=True)
+    user_interest = models.CharField(max_length=100, null=True)
+    career_one= models.CharField(max_length=100, null=True)
+    career_two= models.CharField(max_length=100, null=True)
+    career_three= models.CharField(max_length=100, null=True)
+    career_one_prob= models.IntegerField(null=True)
+    career_two_prob= models.IntegerField(null=True)
+    career_three_prob= models.IntegerField(null=True)
--- recomee__app/views.py
+++ recomee__app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from joblib import load
-# from .models import InputResults
+from .models import InputResults, Username
 
 
 def get_started(request):
     if request.method == 'POST':
         username = request.POST.get('username')
+        user_data = Username(name=username)
+        user_data.save()
+        request.session['username'] = username
         return render(request, 'get_user_input.html')
     return render(request, 'get_started.html')
 
@@ -43,27 +46,41 @@ def career_results(request):
         top_three = {'third_career' : top_three_predictions[2].upper(), 'third_probability' : '{:.2f}'.format(top_three_probabilities[2] * 100)}
         # top_four = {'fourth_career' : top_three_predictions[3].upper(), 'fourth_probability' : '{:.2f}'.format(top_three_probabilities[3] * 100)}
         # username = request.session.get('username')
-        prediction_result = InputResults(
+        combined_results = {**top_one, **top_two, **top_three,}
+
+        first_probability = int(top_three_probabilities[0] * 100)
+        second_probability = int(top_three_probabilities[1] * 100)
+        third_probability = int(top_three_probabilities[2] * 100)
+
+        username = request.session.get('username')
+        user_data = Username.objects.get(name=username)
+        prediction_result = InputResults.objects.create(
+            user_name=user_data,
             user_course=user_course,
             user_industry=user_industry,
             user_skills=user_skills,
-            user_interest =user_interest,
+            user_interest=user_interest,
             career_one=top_three_predictions[0],
             career_two=top_three_predictions[1],
             career_three=top_three_predictions[2],
-            career_one_prob=top_three_probabilities[0],
-            career_two_prob=top_three_probabilities[1],
-            career_three_prob=top_three_probabilities[2])
-            
+            career_one_prob=first_probability,  
+            career_two_prob=second_probability,  
+            career_three_prob=third_probability)             
+        
         prediction_result.save()
     
         if top_three_probabilities[0] < 0.03:
             return render(request, 'no_results.html')
         
-        combined_results = {**top_one, **top_two, **top_three,}
-        
+        context = {
+            'first_probability': int(top_three_probabilities[0] * 100),
+            'second_probability': int(top_three_probabilities[1] * 100),
+            'third_probability': int(top_three_probabilities[2] * 100),
+        }
+
         return render(request,'app_result.html', {
         **combined_results,
+        **context,
         'top_three_predictions': top_three_predictions,
         'top_three_probabilities': top_three_probabilities, 
         })
@@ -75,6 +92,5 @@ def test(request):
 
 
 def display_data(request):
-
-    prediction_result = InputResults.objects.all()
-    return render(request, 'history_page.html', {'prediction_result': prediction_result})
+    all_results = InputResults.objects.all()
+    return render(request, 'history_page.html', {'all_results': all_results})
