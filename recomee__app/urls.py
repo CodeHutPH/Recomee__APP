881ba3d88570c9bf1200363b243fcddbabbf2ffb
Fritz Gerald Castillo <fritzgerald658@gmail.com>
2024-05-17 21:59:56 +0800

    hosting on vercel.com

--- recomee__app/urls.py
+++ recomee__app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,3 +8,6 @@ urlpatterns = [
     path('career_results/', views.career_results, name='career_results'),
     path('display_data/', views.display_data, name='display_data'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
