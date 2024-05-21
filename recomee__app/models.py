30ff497b9f3f11578dcc481da6bd4a09083e112a
Fritz Gerald Castillo <fritzgerald658@gmail.com>
2024-05-21 10:28:29 +0800

    minor changes, dropped database tables

--- recomee__app/models.py
+++ recomee__app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
@@ -6,14 +7,14 @@ class Username(models.Model):
     name = models.CharField(max_length=100, null= True)
 
 class InputResults(models.Model):
-    user_name = models.ForeignKey(Username, on_delete=models.CASCADE, null= True)
+    user_name = models.ForeignKey(Username, on_delete=models.CASCADE, null=True)
     user_course = models.CharField(max_length=100, null=True)
-    user_industry= models.CharField(max_length=100, null=True)
-    user_skills= models.CharField(max_length=100, null=True)
+    user_industry = models.CharField(max_length=100, null=True)
+    user_skills = models.CharField(max_length=100, null=True)
     user_interest = models.CharField(max_length=100, null=True)
-    career_one= models.CharField(max_length=100, null=True)
-    career_two= models.CharField(max_length=100, null=True)
-    career_three= models.CharField(max_length=100, null=True)
-    career_one_prob= models.IntegerField(null=True)
-    career_two_prob= models.IntegerField(null=True)
-    career_three_prob= models.IntegerField(null=True)
+    career_one = models.CharField(max_length=100, null=True)
+    career_two = models.CharField(max_length=100, null=True)
+    career_three = models.CharField(max_length=100, null=True)
+    career_one_prob = models.IntegerField(null=True)
+    career_two_prob = models.IntegerField(null=True)
+    career_three_prob = models.IntegerField(null=True)
